c6be0efd1bec4c89260294d9a0b86c69d1010853
Zech Drinkhall <zechdrink@aol.com>
2019-05-29 14:36:35 -0700

    created a player and room class

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,7 +39,9 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player(room['outside'])
 
+print(player)
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +52,11 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+while True:
+    cmd = input("please input here: ")
+    print(cmd)
+    if cmd == "q":
+        break
+        
+print("The loop has been broken!")
